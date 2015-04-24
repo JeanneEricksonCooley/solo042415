e7d08b0808e0f044294c52c460a16cd5f4c25f12
JeanneEricksonCooley <cool0020@gmail.com>
2015-04-24 14:58:37 -0500

    don't judge my .click lol

---
+++ script.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+
+
+    $(".button").click(function() {
+
+            $.get("text.txt", function (txt) {
+                var newArray = txt.split("\n");
+                console.log(newArray);
+                for (var i = 0; i < newArray.length; i++) {
+                    $(".result").append("<div id=" + i + ">" + newArray[i] + "</div>");
+                    console.log(newArray[i]);
+                }
+
+            });
+    });
+
+});
+
+//josephs
+//$.get("feedback.txt", function(data) {
+//var newArray = data.split("\n");
+//for(var i=0; i < newArray.length; i++) {
+//$("feedback").append("<div id=" + i + ">" newArray[i] + "</div>");
+//}
+//
+
+
+
+//$(#div).html(data)
+//});
+
+
+
+//var lines = data.split("\n");
+//$(".button").click(function() {
+//    for (var i = 0, len = lines.length; i < len; i++) {
+//        save(lines[i]);
+//    }
+//    $( ".result" ).html( data );
+//});
+//
+//
+////alert(data);
+//
+//});
+
+
+
+
+
+
+//test explode code
+// //$('#but-explode').toggle(
+//     function() {
+//         $('#explode').hide('explode', {}, 1000);
+//     },
+//     function() {
+//         $('#explode').show('explode', { pieces: 30 }, 500);
+//     }
+// );
+
+
+//exploding gymnast code
+// $(".hello").toggle(
+// 			function() {
+// 				$("#explode").hide("explode", {}, 1000);
+// 			},
+// 			function() {
+// 				$("#explode").show("explode", { pieces: 30 }, 500)
+// 			}
+// 		);
+//
+// $.get( "text.txt", function( data ) {
+//console.log(data);
+//
